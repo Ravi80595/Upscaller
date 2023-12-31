ebdf91886b078d0ac19ab1de20e0e7113c0a2bb3
Navneet <navneetburman900@gmail.com>
2023-12-31 19:34:17 +0530

    added many

--- client/src/Pages/Home/Carousel.jsx
+++ client/src/Pages/Home/Carousel.jsx
@@ -1,15 +1,18 @@
-
 import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Box, Flex, Image } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Image } from '@chakra-ui/react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import './Carousel.css'; 
+import p1 from '../../Assets/Images/p1.png'
+import Pro from '../../Assets/Images/Pro.gif'
 
 const Carousel = () => {
   const [centerSlideIndex, setCenterSlideIndex] = useState(0);
 
   const CustomNextArrow = ({ onClick }) => (
+    
     <Box
       as={FaChevronRight}
       color="white"
@@ -44,41 +47,42 @@ const Carousel = () => {
   const projects = [
     {
         id: 1,
-        image: 'https://thumbs.dreamstime.com/b/new-projects-word-notebook-59737338.jpg?w=768',
+        image:p1,
         githubLink: 'https://github.com/your-username/project-1',
         deployLink: 'https://deployed-link-1.com',
       },
       {
         id: 2,
-        image: 'https://thumbs.dreamstime.com/b/new-projects-word-notebook-59737338.jpg?w=768',
+        image:p1,
         githubLink: 'https://github.com/your-username/project-2',
         deployLink: 'https://deployed-link-2.com',
       },
       {
         id: 3,
-        image: 'https://thumbs.dreamstime.com/b/new-projects-word-notebook-59737338.jpg?w=768',
+        image:p1,
         githubLink: 'https://github.com/your-username/project-3',
         deployLink: 'https://deployed-link-3.com',
       },
       {
         id: 4,
-        image: 'https://thumbs.dreamstime.com/b/new-projects-word-notebook-59737338.jpg?w=768',
+        image:p1,
         githubLink: 'https://github.com/your-username/project-4',
         deployLink: 'https://deployed-link-4.com',
       },
       {
         id: 5,
-        image: 'https://thumbs.dreamstime.com/b/new-projects-word-notebook-59737338.jpg?w=768',
+        image:p1,
         githubLink: 'https://github.com/your-username/project-5',
         deployLink: 'https://deployed-link-5.com',
       },
       {
         id: 6,
-        image: 'https://thumbs.dreamstime.com/b/new-projects-word-notebook-59737338.jpg?w=768',
+        image:p1,
         githubLink: 'https://github.com/your-username/project-5',
         deployLink: 'https://deployed-link-5.com',
       },
-    ];
+    
+  ];
 
   const settings = {
     dots: true,
@@ -100,26 +104,29 @@ const Carousel = () => {
   };
 
   return (
+    
     <Box position="relative" backgroundColor="black" height="400px">
+        <Center>
+      <Image src={Pro} boxSize="300px" borderRadius="full" />
+      
+    </Center>
+    <br/>
       <Slider {...settings}>
         {projects.map((project, index) => (
           <Flex
             key={project.id}
             alignItems="center"
             justifyContent="center"
-            width="100%" // Set width to 100% to ensure equal spacing
-            outline="none" // Remove the default outline for better styling
+            width="100%"
+            outline="none"
           >
             <Image
               src={project.image}
               alt={`Project ${project.id}`}
-              boxSize={index === centerSlideIndex ? '300px' : '150px'}
-              borderRadius="md"
+              className={index === centerSlideIndex ? 'center-image' : 'side-image'}
               onClick={() => handleImageClick(project.deployLink, index)}
               cursor="pointer"
-              opacity={index !== centerSlideIndex ? 0.8 : 1}
-              transition="opacity 0.3s"
-              _hover={{ opacity: 1 }}
+              _hover={{ filter: 'brightness(1.2)' }} // Neon effect on hover
             />
           </Flex>
         ))}
@@ -129,10 +136,3 @@ const Carousel = () => {
 };
 
 export default Carousel;
-
-
-
-
-
-
-  
